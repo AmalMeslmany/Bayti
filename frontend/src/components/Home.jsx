bfc4caee44d655dbef3545b8e549602a2993a020
Amal Ali Meslmany <amal.alimeslmany@gmail.com>
2026-07-22 15:10:45 +0300

    Initial Bayti project setup

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Welcome to Bayti</h1>;
+}
+
+export default Home;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">Bayti</div>
+
+      <ul className="navbar-links">
+        <li>Home</li>
+        <li>Properties</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
